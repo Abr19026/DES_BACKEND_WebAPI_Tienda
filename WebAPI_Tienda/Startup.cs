32b6a9f32b7c177a7c3e0d6fbbdba9f64572a08d
Abraham Ramírez <abrahammoreno044@gmail.com>
2023-05-22 23:18:26 -0600

    Add project files.

---
+++ WebAPI_Tienda/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace WebAPI_Tienda
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+        // Provee servicios al contenedor
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        // Middleware
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
